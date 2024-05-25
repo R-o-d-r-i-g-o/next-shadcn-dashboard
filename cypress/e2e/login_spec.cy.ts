8b84e76bad1c5439c42a079670e7e82a2b27894e
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-05-25 16:59:56 -0300

    feat: add test cases

--- cypress/e2e/login_spec.cy.ts
+++ cypress/e2e/login_spec.cy.ts
@@ -1,5 +1,23 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('should load login page successfully', () => {
     cy.visit('/');
+    cy.request('/').its('status').should('eq', 200);
+
+    // has man text in page
+    cy.contains('Create an account').should('exist');
+
+    // has continue button
+    cy.contains('Continue With Email').should('exist');
+  });
+
+  it('should sign in successfully', () => {
+    cy.visit('/');
+
+    // Note: there's mocked a value initially
+    // verify if a input text accepts being write.
+    cy.get('input[value="demo@gmail.com"]').clear().type('rodrigo@gmail.com');
+
+    // continue by acessing main dashboard
+    cy.contains('Continue With Email').click();
   });
 });
