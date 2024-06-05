034ac3f8cc948e6362a3cee39e9eee824bdec460
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-06-05 19:37:52 -0300

    .

--- cypress/e2e/login_spec.cy.ts
+++ cypress/e2e/login_spec.cy.ts
@@ -19,5 +19,29 @@ describe('template spec', () => {
 
     // continue by acessing main dashboard
     cy.contains('Continue With Email').click();
+
+    cy.wait(2000);
+    cy.contains('User').click();
+
+    cy.wait(2000);
+    cy.contains('Employee').click();
+
+    cy.get(
+      'button[class="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-9"]'
+    ).click();
+    cy.wait(2000);
+    cy.get('[role="menuitem"]').first().click();
+
+    cy.wait(2000);
+    cy.contains('Profile').click();
+
+    cy.log('O tamanho da tela foi alterado');
+    cy.viewport(1280, 720);
+
+    cy.wait(2000);
+    cy.contains('Kanban').click();
+
+    cy.wait(2000);
+    cy.contains('Login').click();
   });
 });
